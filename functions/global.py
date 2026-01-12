f1302850d28b4dfc47b408c3db2043a1c40d7a0e
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-12 19:48:10 +0530

    global variable

---
+++ functions/global.py
@@ -0,0 +1,11 @@
+# Global variable
+count = 0
+
+def increment():
+    global count      
+    count += 1
+    print("Count inside function:", count)
+increment()
+increment()
+
+print("Count outside function:", count)
