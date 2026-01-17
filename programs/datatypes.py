c161c0b8696e0c2fb79e89b92c4fef8d71916a09
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-17 20:33:58 +0530

    datatypes

---
+++ programs/datatypes.py
@@ -0,0 +1,45 @@
+# Integer
+a = 10
+
+# Float
+b = 5.5
+
+# Complex
+c = 2 + 3j
+
+# String
+name = "Python"
+
+# Boolean
+is_active = True
+
+# List
+numbers = [1, 2, 3]
+
+# Tuple
+point = (4, 5)
+
+# Set
+unique_values = {1, 2, 3}
+
+# Dictionary
+student = {
+    "id": 101,
+    "name": "Vinnu",
+    "marks": 85
+}
+
+# None type
+result = None
+
+# Printing all data types
+print("Integer:", a)
+print("Float:", b)
+print("Complex:", c)
+print("String:", name)
+print("Boolean:", is_active)
+print("List:", numbers)
+print("Tuple:", point)
+print("Set:", unique_values)
+print("Dictionary:", student)
+print("None type:", result)
