74ff9f60c43e61dbdefcaac256a5d582f93c9791
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-18 22:07:14 +0530

    convesion

---
+++ programs/exp9.py
@@ -0,0 +1,13 @@
+# Example 1: String to Integer (Invalid)
+x = "abc"
+print("Cannot convert 'abc' to int")
+a = "10.5"
+print("Cannot convert '10.5' directly to int")
+
+b = float(a)
+print("Converted to float:", b)
+lst = [1, 2, 3]
+print("Cannot convert list to int")
+
+val = None
+print("Cannot convert None to int")
