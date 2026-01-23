27818e47a655ed157ea84a06f41191f06763c2b4
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-23 21:49:55 +0530

    user input

---
+++ functions/program1.py
@@ -0,0 +1,10 @@
+# Function definition
+def square(num):
+    return num * num
+
+# Taking input from user
+n = int(input("Enter a number: "))
+
+# Function call
+result = square(n)
+print("Square:", result)
