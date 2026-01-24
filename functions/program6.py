7b134ba5e05f767fb8feb19dc6a1de2a5adb3ff7
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-24 21:27:44 +0530

    marks

---
+++ functions/program6.py
@@ -0,0 +1,22 @@
+# Student Marks Grading Program
+
+marks = int(input("Enter student marks (0–100): "))
+
+if marks < 0 or marks > 100:
+    print("Invalid marks")
+
+elif marks >= 90:
+    print("Grade: A")
+
+elif marks >= 75:
+    print("Grade: B")
+
+elif marks >= 60:
+    print("Grade: C")
+
+elif marks >= 40:
+    print("Grade: D")
+
+else:
+    print("Grade: Fail")
+
