1930fbc1ab810e62995627e5de92b8d94dcd4420
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-01-31 21:11:50 +0530

    add

---
+++ oops concepts/example2.py
@@ -0,0 +1,39 @@
+# Parent class
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name          # Encapsulation
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Deposited:", amount)
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print("Withdrawn:", amount)
+        else:
+            print("Insufficient balance")
+
+    def display(self):
+        print("Account Holder:", self.name)
+        print("Balance:", self.balance)
+
+
+# Child class (Inheritance)
+class SavingsAccount(BankAccount):
+    def withdraw(self, amount):     # Polymorphism
+        if amount > 10000:
+            print("Withdrawal limit exceeded")
+        else:
+            super().withdraw(amount)
+
+
+# Creating object
+acc = SavingsAccount("Amit", 20000)
+
+acc.display()
+acc.deposit(5000)
+acc.withdraw(12000)
+acc.withdraw(8000)
+acc.display()
