26599ac0ad2480320fdbb00a2e4e4e334c6605e7
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-07 22:56:33 +0530

    new  file

---
+++ new/new.py
@@ -0,0 +1,17 @@
+# List data structure example
+
+marks = [78, 85, 90, 66, 85]
+
+# Display marks
+print("Marks:", marks)
+
+# Add a new mark
+marks.append(92)
+
+# Find total and average
+total = sum(marks)
+average = total / len(marks)
+
+print("Updated Marks:", marks)
+print("Total:", total)
+print("Average:", average)
