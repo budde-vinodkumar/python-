9511bdb892662f7b3d8057428ab41e056c07b870
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-11 18:48:37 +0530

    modified data

--- new/queue.py
+++ new/queue.py
@@ -2,17 +2,14 @@
 
 queue = []
 
-# Enqueue operations
 queue.append(10)
 queue.append(20)
 queue.append(30)
 
 print("Queue after enqueue:", queue)
 
-# Dequeue operation
 queue.pop(0)
 print("Queue after dequeue:", queue)
 
-# Front element
 front = queue[0]
 print("Front element:", front)
