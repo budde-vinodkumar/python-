e809e2c6b01839fba21ce8218a64854825fdb312
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-18 22:53:19 +0530

    loops

--- loooping statements/loop2.py
+++ loooping statements/loop2.py
@@ -13,4 +13,11 @@ while num <= 5:
         break             # stop loop at 4
     print(num)
     num += 1
-print("Loop ended.")    
+print("Loop ended.")   
+
+numbers = [10, 20, 30, 40]
+print("\nUsing FOR loop to iterate over a list:")   
+for n in numbers:
+    print(n)    
+print("Final List:", numbers)
+print("Length of List:", len(numbers))
