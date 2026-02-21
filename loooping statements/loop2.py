755afdc78b99e40e76f7c1479db15e309ec48cc6
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-21 23:53:40 +0530

    add

--- loooping statements/loop2.py
+++ loooping statements/loop2.py
@@ -6,14 +6,6 @@ for i in range(1, 6):
         continue          # skip 3
     print(i)
 
-print("\nUsing WHILE loop:")
-num = 1
-while num <= 5:
-    if num == 4:
-        break             # stop loop at 4
-    print(num)
-    num += 1
-print("Loop ended.")   
 
 numbers = [10, 20, 30, 40]
 print("\nUsing FOR loop to iterate over a list:")   
