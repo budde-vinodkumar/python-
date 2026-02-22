f37649531d4d997934c159e09675c911fc88672f
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-22 22:58:20 +0530

    changes

--- loooping statements/loop2.py
+++ loooping statements/loop2.py
@@ -5,11 +5,3 @@ for i in range(1, 6):
     if i == 3:
         continue          # skip 3
     print(i)
-
-
-numbers = [10, 20, 30, 40]
-print("\nUsing FOR loop to iterate over a list:")   
-for n in numbers:
-    print(n)    
-print("Final List:", numbers)
-print("Length of List:", len(numbers))
