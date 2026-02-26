1dc8398bd5f40d4f6cf99085229a52277e10cf31
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-26 22:44:58 +0530

    addfile

---
+++ lists/list2.py
@@ -0,0 +1,17 @@
+vinnu=[1,2,3,4,5]
+print(vinnu[0])          # first element
+print(vinnu[-1])         # last element
+vinnu.append(6)
+vinnu.extend([7, 8])
+vinnu.insert(1, 1.5)
+vinnu[2] = 2.5
+vinnu.remove(4)
+vinnu.pop()
+vinnu.pop(0)
+temp_list = vinnu.copy()
+temp_list.clear()
+print(vinnu.index(3))
+print(vinnu.count(2))
+print(vinnu.count(10))
+vinnu.append(3)
+print(vinnu.count(3))
