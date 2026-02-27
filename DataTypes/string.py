7dfd266a386a22bb59661c90e8e4587e2b384d7a
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-02-27 23:33:36 +0530

    string

---
+++ DataTypes/string.py
@@ -0,0 +1,11 @@
+a="vinnu"
+b="vinnu"
+print(a[0])          # first character
+print(a[-1])         # last character
+print(a[1:4])        # substring from index 1 to 3
+print(a.upper())     # convert to uppercase
+print(a.lower())     # convert to lowercase
+print(a.replace("v", "V"))  # replace 'v' with 'V'
+print(a.split("n"))  # split the string by 'n'
+print(a.find("n"))   # find the index of first occurrence of 'n'
+print(a.count("n"))  # count occurrences of 'n'
