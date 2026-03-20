9c25375560965d8171cd652e274e5fc34657fbc0
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-03-20 21:42:10 +0530

    commit

--- loooping statements/lop3.py
+++ loooping statements/lop3.py
@@ -2,9 +2,5 @@ a=20
 b=200
 c=2000
 print("The numbers are",a,b,c)
-print("Using FOR loop:")
-for i in range(1,6):
-    if i==3:
-        continue          # skip 3
-    print(i)
-    
+print("The numbers are",a,b,c,sep=";")  # sep is used to specify the separator between values
+print("The numbers are",a,b,c,sep="; ",end=".")  #
