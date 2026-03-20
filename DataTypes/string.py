d1330c71b91f83e68560f66af4c8b7c0f765baae
budde-vinodkumar <buddevinodkumar@gmail.com>
2026-03-20 21:40:27 +0530

    add

--- DataTypes/string.py
+++ DataTypes/string.py
@@ -4,7 +4,3 @@ print(a[0])
 print(a.endswith("u"))    # check if string ends with 'u'
 print(a.isalpha())  # check if all characters are alphabetic
 print(a.isdigit())  # check if all characters are digits
-print(a.strip())    # remove leading and trailing whitespace
-print(a.center(20)) # center the string within a width of 20
-print(a.ljust(20))  
-print(a.rjust(20))  
